d09a574c6b676b0217bc3806c3b313d0553991b6
JayaChandra789 <bagamjaya789@gmail.com>
2024-12-10 06:48:50 +0530

    StudentDAO.java

--- StudentDAO.java
+++ StudentDAO.java
@@ -1,5 +1,4 @@
 package com.databasemanagament.studentmanagment.dao;
-
 import java.sql.*;
 
 import com.databasemanagament.studentmanagment.entity.StudentEntities; /*Mandate to import package of entities or were you stored values in entity program as it is encapsulated / private*/
@@ -14,17 +13,16 @@ public class StudentDAO
 			if (conObj != null) 
 			{
 				/*----Creating PreparedStatement---*/
-				PreparedStatement stmtObj = conObj.prepareStatement("insert into values(?,?,?,?)");
+				PreparedStatement preStmtObj = conObj.prepareStatement("INSERT INTO VALUES(?,?,?,?)");
 				/*------------------------------------------------------------------------------*/
 				/*-----Setting data into prepared statement------*/
-				stmtObj.setString(1, studentObj.getStdName());
-				stmtObj.setInt(2, studentObj.getAge());
-				stmtObj.setInt(3, studentObj.getStdId());
-				stmtObj.setString(4, studentObj.getAddress());
-
+				preStmtObj.setString(1, studentObj.getStdName());  //Invoked from getter and setter from entity class
+				preStmtObj.setInt(2, studentObj.getStdAge());      //Invoked from getter and setter from entity class
+				preStmtObj.setInt(3, studentObj.getStdId()); 		//Invoked from getter and setter from entity class
+				preStmtObj.setString(4, studentObj.getStdAddress()); //Invoked from getter and setter from entity class
 				/*----------------------------------------------------------*/
 				/*--------To execute query-------*/
-				rows = stmtObj.executeUpdate();
+				rows = preStmtObj.executeUpdate();
 				return rows;
 			} 
 			else 
@@ -38,4 +36,55 @@ public class StudentDAO
 			return rows;
 		}
 	}
+	
+	/* Method to update Age */
+    public int updateRoll(Connection conObj, int stdId, int newAge) 
+    {
+        int rows = 0;
+        try 
+        {
+            if (conObj != null) 
+            {
+                PreparedStatement preStmtObj = conObj.prepareStatement("UPDATE student SET age = ? WHERE stdAge = ?");
+                preStmtObj.setInt(1, newAge);
+                preStmtObj.setInt(2, stdId);
+                rows = preStmtObj.executeUpdate();
+                return rows;
+            } 
+            else 
+            {
+                return rows;
+            }
+        } 
+        catch (SQLException SQLObj) 
+        {
+            System.out.println(SQLObj);
+            return rows;
+        }
+    }
+    /* Method to update Address */
+    public int updateAddress(Connection conObj, int stdId, String newAddress) 
+    {
+        int rows = 0;
+        try 
+        {
+            if (conObj != null) 
+            {
+                PreparedStatement preStmtObj = conObj.prepareStatement("UPDATE student SET address = ? WHERE stdId = ?");
+                preStmtObj.setString(1, newAddress);
+                preStmtObj.setInt(2, stdId);
+                rows = preStmtObj.executeUpdate();
+                return rows;
+            } 
+            else 
+            {
+                return rows;
+            }
+        } 
+        catch (SQLException SQLObj) 
+        {
+            System.out.println(SQLObj);
+            return rows;
+        }
+    }
 }
